564a43bc713fe2045f8eaef86c7345cb1c016f2c
Mohit1215 <31873157+Mohit1215@users.noreply.github.com>
2018-11-02 09:56:53 -0400

    another commit

---
+++ Code_for_OO/ReserveImplement.java
@@ -0,0 +1,24 @@
+
+
+public class ReserveImplement implements Reserve{
+    String flightNo ;
+    int seatNo;
+    String fare;
+    String destination;
+	 
+	public void setFlightNo(String i) {
+		flightNo = i;
+	}
+
+	public void setSeat(int i) {
+	seatNo = i;		
+	}
+
+	public void setfare(String i) {
+		fare = i;		
+	}
+
+	public void setDestination(String i) {
+		destination = i;	
+	}
+}
